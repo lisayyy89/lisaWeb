b919039ed76110a0bae07d6f10293cde5ac6c3c1
lisayyy89 <112566411+lisayyy89@users.noreply.github.com>
2023-12-03 22:58:31 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.4
+    });
+
+    document.querySelectorAll('.section').forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    console.log('click disparado')
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
+
